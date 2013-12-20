4e3f192749e8f1c569a51aa1e9bcfc8289964979
Wil <wil@server.fake>
2013-12-20 19:34:23 +0900

    Clean up "using" statements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-//using System.Windows.Shapes;
-using System.IO;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace pngPalette
 {
